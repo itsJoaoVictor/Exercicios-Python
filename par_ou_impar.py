b6173a21920a0bd66473fe1687938759660117c8
itsJoaoVictor <jvgf98@gmail.com>
2021-11-23 21:20:34 -0300

    first commit

---
+++ par_ou_impar.py
@@ -0,0 +1,13 @@
+print("Este programa irá dizer se o número que você pensou é ímpar ou par")
+num = int(input("Por favor digite um número "))
+
+p_i = num % 2
+
+if p_i == 0:{
+    print("Este número é par")
+    
+}
+
+else:{
+    print("Este número é ímpar")
+}
